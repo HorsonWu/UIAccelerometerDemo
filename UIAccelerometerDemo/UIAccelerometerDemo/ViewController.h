f835096b93ff260be8387aa3dfd5101b77e01697
wuyihao <wuyihao@revenco.com>
2015-11-24 09:25:18 +0800

    添加工程

---
+++ UIAccelerometerDemo/UIAccelerometerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIAccelerometerDemo
+//
+//  Created by HorsonWu on 15/11/24.
+//  Copyright © 2015年 elovega. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UIAccelerometerDemo/UIAccelerometerDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UIAccelerometerDemo
+//
+//  Created by HorsonWu on 15/11/24.
+//  Copyright © 2015年 elovega. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
