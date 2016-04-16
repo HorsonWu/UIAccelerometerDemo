0ca1cbd43ec6e63bdde8a266d075181ddc07cc77
wuyihao <wuyihao@revenco.com>
2016-04-16 11:06:25 +0800

    整理好各感应功能点

--- UIAccelerometerDemo/UIAccelerometerDemo/ViewController.m
+++ UIAccelerometerDemo/UIAccelerometerDemo/ViewController.m
@@ -6,9 +6,19 @@
 //  Copyright © 2015年 elovega. All rights reserved.
 //
 
+
+/*加速度计和陀螺仪
+ *  1.检测设备的晃动
+ *   a.用加速度计（UIAccelerometer（ios5.0以后不推荐使用），CoreMotion框架）
+ *   b.用UIResponder类检测
+ *  2.设备的方向
+ *  3.陀螺仪
+ */
+
+
 #import "ViewController.h"
 #import <CoreMotion/CoreMotion.h>
-@interface ViewController ()<UIAlertViewDelegate>
+@interface ViewController ()<UIAlertViewDelegate,UIAccelerometerDelegate>
 {
     //数据滤波后的数值
     UIAccelerationValue filteredAccelX;
@@ -21,54 +31,41 @@
 
 @implementation ViewController
 
+
+#pragma mark == 使用UIAccelerometer检测手机晃动
+#pragma mark 第一种方法（UIAccelerometer对象检测手机是否晃动，iOS5.0后不推荐使用）
+//-(void)initAccelerometer{
+//    UIAccelerometer *accelerometer = [UIAccelerometer sharedAccelerometer];
+//    accelerometer.updateInterval =0.1;
+//    accelerometer.delegate =self;
+//}
+//// 实现UIAccelerometer实例的代理方法
+//-(void)accelerometer:(UIAccelerometer *)accelerometer didAccelerate:(UIAcceleration *)acceleration
+//{ // iOS5.0 以后不推荐使用
+//    if (fabs(acceleration.x)>2.0 ||fabs(acceleration.y)>2.0 ||fabs(acceleration.z)>2.0) {
+//        NSLog(@"检测到晃动");
+//    }
+//}
+#pragma mark 第二种方法（使用CoreMotion框架，根据acceleration值来判断手机是否晃动）
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
     [self accelerometerOfCoreMotionType];
-//    [self gyroOfCoreMotionType];
-    
 }
--(void)viewWillAppear:(BOOL)animated{
-    [super viewWillAppear:YES];
-//    [self getTheDeviceOrientation];
-}
+
 
 -(void)viewWillDisappear:(BOOL)animated{
     [super viewWillDisappear:YES];
-//    [self removeNotificationOfGetDeviceOrientation];
-//    [self resignFirstResponder];
-    
-    
-//    [self.motionManager stopGyroUpdates];
-//    [self.motionManager stopAccelerometerUpdates];
-//    [self.motionManager stopDeviceMotionUpdates];
+    [self.motionManager stopAccelerometerUpdates];
+    [self.motionManager stopDeviceMotionUpdates];
 }
 
--(void)viewDidAppear:(BOOL)animated
-{
-    [super viewDidAppear:YES];
-//    [self becomeFirstResponder];
-}
 
--(BOOL)canBecomeFirstResponder{
-    return YES;
-}
 #pragma mark -------------------------------- 加速度计 -------------------------------
 //以CoreMotion方式获取加速度计的参数 (ios 5系统以后)
 -(void)accelerometerOfCoreMotionType{
     self.motionManager = [CMMotionManager new];
     self.motionManager.accelerometerUpdateInterval = 0.1;
     [self startShake];
-}
-
-//- (void)alertViewCancel:(UIAlertView *)alertView{
-//}
-
-- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
-    if (buttonIndex == 0) {
-        [self startShake];
-
-    }
 }
 -(void)startShake{
     if ([self.motionManager isAccelerometerAvailable]) {
@@ -109,112 +106,156 @@
                                                      }
                                                  }];
     }
-
+    
 }
-
-//感知设备方向
--(void)getTheDeviceOrientation{
-    [[UIDevice currentDevice] beginGeneratingDeviceOrientationNotifications];
-    [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(receivedRotation:) name:UIDeviceOrientationDidChangeNotification
-                                               object:nil];
-}
--(void)receivedRotation:(NSNotification *)notification{
-    UIDevice *device = [UIDevice currentDevice];
-    switch (device.orientation) {
-        case UIDeviceOrientationPortrait:
-            NSLog(@"竖直向上");
-            break;
-        case UIDeviceOrientationPortraitUpsideDown:
-            NSLog(@"竖直向下");
-            break;
-        case UIDeviceOrientationLandscapeLeft:
-            NSLog(@"水平向左");
-            break;
-        case UIDeviceOrientationLandscapeRight:
-            NSLog(@"水平向右");
-            break;
-        case UIDeviceOrientationFaceUp:
-            NSLog(@"面朝上");
-            break;
-        case UIDeviceOrientationFaceDown:
-            NSLog(@"面朝下");
-            break;
-        case UIDeviceOrientationUnknown:
-            NSLog(@"未知");
-            break;
-        default:
-            NSLog(@"未知");
-            break;
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    if (buttonIndex == 0) {
+        [self startShake];
+        
     }
 }
+#pragma mark == 用UIResponder类检测手机晃动（可以用模拟机测试）
 
--(void)removeNotificationOfGetDeviceOrientation{
-    [[NSNotificationCenter defaultCenter]removeObserver:self];
-    [[UIDevice currentDevice] endGeneratingDeviceOrientationNotifications];
-}
+//- (void)viewDidLoad {
+//    [super viewDidLoad];
+//    [[UIApplication sharedApplication]setApplicationSupportsShakeToEdit:YES];
+//}
+
+//-(void)viewDidAppear:(BOOL)animated
+//{
+//    [super viewDidAppear:YES];
+//    [self becomeFirstResponder];
+//}
+//
+//-(BOOL)canBecomeFirstResponder{
+//    return YES;
+//}
+//-(void)viewWillDisappear:(BOOL)animated{
+//    [self resignFirstResponder];
+//}
+
+#pragma mark == 晃动检测的处理
+
+//-(void)motionBegan:(UIEventSubtype)motion withEvent:(UIEvent *)event{
+//    if (motion == UIEventSubtypeMotionShake) {
+//        NSLog(@"晃动开始");
+//    }
+//}
+//-(void)motionCancelled:(UIEventSubtype)motion withEvent:(UIEvent *)event{
+//    if (motion == UIEventSubtypeMotionShake) {
+//        NSLog(@"晃动取消");
+//    }
+//}
+//-(void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event{
+//    if (motion == UIEventSubtypeMotionShake) {
+//        NSLog(@"晃动结束");
+//    }
+//}
+
+#pragma mark == 感知设备方向
+//-(void)viewWillAppear:(BOOL)animated{
+//    [super viewWillAppear:YES];
+//    [self getTheDeviceOrientation];
+//}
+//
+//-(void)getTheDeviceOrientation{
+//    [[UIDevice currentDevice] beginGeneratingDeviceOrientationNotifications];
+//    [[NSNotificationCenter defaultCenter] addObserver:self
+//                                             selector:@selector(receivedRotation:) name:UIDeviceOrientationDidChangeNotification
+//                                               object:nil];
+//}
+//-(void)receivedRotation:(NSNotification *)notification{
+//    UIDevice *device = [UIDevice currentDevice];
+//    switch (device.orientation) {
+//        case UIDeviceOrientationPortrait:
+//            NSLog(@"竖直向上");
+//            break;
+//        case UIDeviceOrientationPortraitUpsideDown:
+//            NSLog(@"竖直向下");
+//            break;
+//        case UIDeviceOrientationLandscapeLeft:
+//            NSLog(@"水平向左");
+//            break;
+//        case UIDeviceOrientationLandscapeRight:
+//            NSLog(@"水平向右");
+//            break;
+//        case UIDeviceOrientationFaceUp:
+//            NSLog(@"面朝上");
+//            break;
+//        case UIDeviceOrientationFaceDown:
+//            NSLog(@"面朝下");
+//            break;
+//        case UIDeviceOrientationUnknown:
+//            NSLog(@"未知");
+//            break;
+//        default:
+//            NSLog(@"未知");
+//            break;
+//    }
+//}
+//-(void)viewWillDisappear:(BOOL)animated{
+//    [super viewWillDisappear:YES];
+//    [self removeNotificationOfGetDeviceOrientation];
+//}
+//-(void)removeNotificationOfGetDeviceOrientation{
+//    [[NSNotificationCenter defaultCenter]removeObserver:self];
+//    [[UIDevice currentDevice] endGeneratingDeviceOrientationNotifications];
+//}
 
 
 #pragma mark -------------------------------- 陀螺仪 -------------------------------
 
--(void)gyroOfCoreMotionType{
-    self.motionManager = [CMMotionManager new];
-    self.motionManager.accelerometerUpdateInterval = 0.1;
-    if ([self.motionManager isGyroAvailable]) {
-        [self.motionManager startGyroUpdatesToQueue:[NSOperationQueue mainQueue] withHandler:^(CMGyroData * _Nullable gyroData, NSError * _Nullable error) {
-            if (error) {
-                [self.motionManager stopGyroUpdates];
-            }
-            else
-            {
-                CMRotationRate rotate = gyroData.rotationRate;
-                NSLog(@"x = %f,y = %f,z = %f",rotate.x, rotate.y,rotate.z);
-                CGFloat x =  [self calculatehighPassFilteredDataWithNewAcceleration:filteredAccelX andPreviousValue:rotate.x];
-                CGFloat y =  [self calculatehighPassFilteredDataWithNewAcceleration:filteredAccelX andPreviousValue:rotate.y];
-                CGFloat z =  [self calculatehighPassFilteredDataWithNewAcceleration:filteredAccelX andPreviousValue:rotate.z];
-                filteredAccelX = x;
-                filteredAccelY = y;
-                filteredAccelZ = z;
-                
-                 NSLog(@"filteredAccelX = %f,filteredAccelY = %f,filteredAccelZ = %f",filteredAccelX, filteredAccelY,filteredAccelZ);
-            }
-        }];
-    }
-    
-}
+//- (void)viewDidLoad {
+//    [super viewDidLoad];
+//    [self gyroOfCoreMotionType];
+//}
+//-(void)viewWillDisappear:(BOOL)animated{
+//    [super viewWillDisappear:YES];
+//    [self.motionManager stopGyroUpdates];
+//}
 
 
-#pragma mark -------------------------------- 数据滤波的处理 -------------------------------
-//低通滤波计算  目的:找出设备的方向
--(CGFloat)calculateLowPassFilteredDataWithNewAcceleration:(CGFloat)newAcceleration andPreviousValue:(CGFloat)previousValue{
-    CGFloat lowPassFiltered = (newAcceleration * 0.1)+ previousValue * (1.0 - 0.1);
-    return lowPassFiltered;
-}
+//-(void)gyroOfCoreMotionType{
+//    self.motionManager = [CMMotionManager new];
+//    self.motionManager.accelerometerUpdateInterval = 0.1;
+//    if ([self.motionManager isGyroAvailable]) {
+//        [self.motionManager startGyroUpdatesToQueue:[NSOperationQueue mainQueue] withHandler:^(CMGyroData * _Nullable gyroData, NSError * _Nullable error) {
+//            if (error) {
+//                [self.motionManager stopGyroUpdates];
+//            }
+//            else
+//            {
+//                CMRotationRate rotate = gyroData.rotationRate;
+//                NSLog(@"x = %f,y = %f,z = %f",rotate.x, rotate.y,rotate.z);
+//                CGFloat x =  [self calculatehighPassFilteredDataWithNewAcceleration:filteredAccelX andPreviousValue:rotate.x];
+//                CGFloat y =  [self calculatehighPassFilteredDataWithNewAcceleration:filteredAccelX andPreviousValue:rotate.y];
+//                CGFloat z =  [self calculatehighPassFilteredDataWithNewAcceleration:filteredAccelX andPreviousValue:rotate.z];
+//                filteredAccelX = x;
+//                filteredAccelY = y;
+//                filteredAccelZ = z;
+//                
+//                 NSLog(@"filteredAccelX = %f,filteredAccelY = %f,filteredAccelZ = %f",filteredAccelX, filteredAccelY,filteredAccelZ);
+//            }
+//        }];
+//    }
+//    
+//}
+//
+//
+//#pragma mark -------------------------------- 数据滤波的处理 -------------------------------
+////低通滤波计算  目的:找出设备的方向
+//-(CGFloat)calculateLowPassFilteredDataWithNewAcceleration:(CGFloat)newAcceleration andPreviousValue:(CGFloat)previousValue{
+//    CGFloat lowPassFiltered = (newAcceleration * 0.1)+ previousValue * (1.0 - 0.1);
+//    return lowPassFiltered;
+//}
+//
+////高通滤波计算  目的:检测用户的移动
+//-(CGFloat)calculatehighPassFilteredDataWithNewAcceleration:(CGFloat)newAcceleration andPreviousValue:(CGFloat)previousValue{
+//    CGFloat highPassFiltered = newAcceleration - (newAcceleration * 0.1)+ previousValue * (1.0 - 0.1);
+//    return highPassFiltered;
+//}
 
-//高通滤波计算  目的:检测用户的移动
--(CGFloat)calculatehighPassFilteredDataWithNewAcceleration:(CGFloat)newAcceleration andPreviousValue:(CGFloat)previousValue{
-    CGFloat highPassFiltered = newAcceleration - (newAcceleration * 0.1)+ previousValue * (1.0 - 0.1);
-    return highPassFiltered;
-}
 
-
-#pragma mark -------------------------------- 晃动检测的处理 -------------------------------
-
--(void)motionBegan:(UIEventSubtype)motion withEvent:(UIEvent *)event{
-    if (motion == UIEventSubtypeMotionShake) {
-        NSLog(@"晃动开始");
-    }
-}
--(void)motionCancelled:(UIEventSubtype)motion withEvent:(UIEvent *)event{
-    if (motion == UIEventSubtypeMotionShake) {
-        NSLog(@"晃动取消");
-    }
-}
--(void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event{
-    if (motion == UIEventSubtypeMotionShake) {
-        NSLog(@"晃动结束");
-    }
-}
 
 #pragma mark ----------------------------------------------------------------
 - (void)didReceiveMemoryWarning {
